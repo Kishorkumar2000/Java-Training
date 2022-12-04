4dd3b22fd88570b5097347e417a66aa9e82394b2
Kishorkumar2000 <119475600+Kishorkumar2000@users.noreply.github.com>
2022-12-04 22:00:40 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,25 @@
+class Demo
+{
+	// 4) Print the default values of all the primitive data types
+	   static int a;
+		static byte b;
+		static float f;
+		static short s;
+		static boolean d;
+		static double e;
+		static long l;
+		static char c;
+	public static void main(String[] args) 
+	{
+		
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(f);
+		System.out.println(s);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(c);
+		System.out.println(l);
+	}
+}
